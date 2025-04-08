f2f29f023b026d6933433bef37e0c0d1e7bfe79f
Bgarcia117 <bryant.garcia.1771@gmail.com>
2025-04-07 22:41:13 -0400

    Started header file for MinHeap

--- Data-Structures/include/MinHeap.h
+++ Data-Structures/include/MinHeap.h
@@ -1,10 +1,22 @@
 #pragma once
 
-struct MinHeap {
-	int* element;
+class MinHeap{
+private:
+	int* elements;
 	int numOfElements;
 	int capacity;
 
+	int Parent(int i) { return (i - 1) / 2; }
+	int LeftChild(int i) { return (2 * i) + 1; }
+	int RightChild(int i) { return (2 * i) + 2; }
+
+	void swap(int& a, int& b) {
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+
+public:
 	// Default argument constructor, different value can be passed
 	MinHeap(int startCapacity = 10) {
 		elements = new numOfElements[startCapacity];
@@ -13,6 +25,17 @@ struct MinHeap {
 	}
 
 	~MinHeap() { delete[] element; }
+
+	// Observer Functions
+	bool IsFull() { return numOfElements == capacity; }
+	bool IsEmpty() { return numOfElements == 0; }
+
+	// Transformer functions
+	void MakeEmpty() { numOfElements = 0; } 
+	void Insert(int value);
+	void HeapifyUp(int index);
+	void HeapifyDown(int index);
+	
 };
 
 class PriorityQueue {
@@ -20,5 +43,5 @@ private:
 	MinHeap heap;
 
 public:
-	PriorityQueue(int startCapacity = 10) : heap(startCapacity);
+	PriorityQueue(int startCapacity = 10) : heap(startCapacity) {}
 };
